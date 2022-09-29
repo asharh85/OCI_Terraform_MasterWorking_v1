2f1cd5fb2f0b373ac3348d032adb5c1782b24384
asharh85 <asharh85@gmail.com>
2022-09-29 01:11:22 -0400

    Update networking.tf

--- networking.tf
+++ networking.tf
@@ -1,19 +1,19 @@
 resource "oci_core_vcn" "demo-box" {
   cidr_block     = "172.16.0.0/16"
-  compartment_id = var.demo_compartment_ocid
+  compartment_id = var.compartment_id
   dns_label      = "demo"
   display_name   = "Demo Box"
 }
 
 resource "oci_core_internet_gateway" "demo-box" {
-  compartment_id = var.demo_compartment_ocid
+  compartment_id = var.compartment_id
   display_name   = "Demo Box"
   vcn_id         = oci_core_vcn.demo-box.id
   enabled        = true
 }
 
 resource "oci_core_route_table" "demo-box" {
-  compartment_id = var.demo_compartment_ocid
+  compartment_id = var.compartment_id
   vcn_id         = oci_core_vcn.demo-box.id
   display_name   = "Demo Box"
 
@@ -25,7 +25,7 @@ resource "oci_core_route_table" "demo-box" {
 
 resource "oci_core_security_list" "demo-box-incoming" {
   display_name   = "Incoming traffic for Demo Box"
-  compartment_id = var.demo_compartment_ocid
+  compartment_id = var.compartment_id
   vcn_id         = oci_core_vcn.demo-box.id
 
   egress_security_rules {
@@ -71,7 +71,7 @@ resource "oci_core_security_list" "demo-box-incoming" {
 
 resource "oci_core_subnet" "demo-box" {
   cidr_block        = oci_core_vcn.demo-box.cidr_block
-  compartment_id    = var.demo_compartment_ocid
+  compartment_id    = var.compartment_id
   vcn_id            = oci_core_vcn.demo-box.id
   route_table_id    = oci_core_route_table.demo-box.id
 
